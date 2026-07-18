c734fa3457bcfc661af9b5f6b6fa4ff43f2c26ee
mineproness-org <gcfreegiftcards@gmail.com>
2026-07-18 10:31:20 +0600

    ddd

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import { Linear as li2 } from "./src/Linear/index.js";
 import { Softmax as softmax2 } from "./src/Softmax/index.js";
 import { crossEntropy } from "./src/crossEntropy/index.js";
 import {tools} from './src/tools/index.js'
+import {ReLU} from './src/ReLU/index.js'
 export const Tokenizer = tokenizer
 export const Embedding = embeddding2
 export const Linear = li2
 export const Softmax = softmax2
 export const CrossEntropy = crossEntropy
 export const Tools = tools
+export const ReLu = ReLU
