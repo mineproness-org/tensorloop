373a02980cbcb94b2c4978697e2adf0cdd551d67
mineproness-org <gcfreegiftcards@gmail.com>
2026-07-20 21:30:09 +0600

    Optimize some Softmax

--- src/Softmax/index.js
+++ src/Softmax/index.js
@@ -3,10 +3,10 @@ export class Softmax{
         const max = Math.max(...xLi)
         const exp = xLi.map((e)=> Math.exp(e - max))
         const sum = exp.reduce((a,b)=> a + b , 0)
-        return exp.map((e)=> e / sum)
+        return new Float32Array(exp.map((e)=> e / sum))
     }
     backward(probs , target){
-        const pro = [...probs]
+        const pro = new Float32Array([...probs])
         pro[target] -= 1
         return pro
     }
